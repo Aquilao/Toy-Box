2bcef905959211fa70bfd45f484534311d3b8c8a
Aquilao <aquilao@outlook.com>
2019-04-08 15:40:31 +0800

    [^] Add progress bar for  http_option_scan

--- http_options_scan/http_options_scan.py
+++ http_options_scan/http_options_scan.py
@@ -8,21 +8,11 @@ from optparse import OptionParser
 from concurrent.futures import ThreadPoolExecutor
 import IPy
 import re
+import math
+import sys
 
-
-#!/usr/bin/env python3
-# http dangerous option scanner
-# by Aquilao
-
-import threading
-import requests
-from optparse import OptionParser
-import IPy
-import re
 
 #lock = threading.Lock()
-threads = []
-executor = ThreadPoolExecutor(max_workers=200)
 
 # 交互显示
 def mainPage():
@@ -32,21 +22,30 @@ def mainPage():
     #    python3 http_options_scan.py -u http://192.168.1.100
     #    python3 http_options_scan.py -u http://192.168.1.0/24
     #""")
+    banner = """
+     _     _   _                       _   _                                       
+    | |__ | |_| |_ _ __     ___  _ __ | |_(_) ___  _ __  ___   ___  ___ __ _ _ __  
+    | '_ \| __| __| '_ \   / _ \| '_ \| __| |/ _ \| '_ \/ __| / __|/ __/ _` | '_ \ 
+    | | | | |_| |_| |_) | | (_) | |_) | |_| | (_) | | | \__ \ \__ \ (_| (_| | | | |
+    |_| |_|\__|\__| .__/   \___/| .__/ \__|_|\___/|_| |_|___/ |___/\___\__,_|_| |_|
+                  |_|           |_|                                                
+     """
+    print(banner)
     option = OptionParser()
     option.add_option('-u', '--url', default=False, help='-u [http(s)://domain/ip(/mask length)]')
+    option.add_option('-t', '--thread', default=256, help='-t <thread_number>, default 256')
     option.add_option('-v', '--view', default=False, action='store_true', help='show more message')
     option.add_option('-d', '--dangerous', default=False, action='store_true', help='only show dangerous message')
     options, args = option.parse_args()
     url = options.url
-    global view, dangerous
+    global view, dangerous, thread
     view = options.view
     dangerous = options.dangerous
+    thread = int(options.thread)
     if url == False:
         option.print_help()
         exit()
-    print("Please wait...")
     return url
-    
 
 # 获取 HTTP Header
 def getHTTPHead(url):
@@ -91,6 +90,21 @@ def urlSplit(url):
         urls.append(url)
     return urls
 
+# 进度条
+def progress_bar(portion, total, info):
+    part = total / 50
+    count = math.ceil(portion / part)
+    #sys.stdout.write('\r')
+    sys.stdout.write(' ' * 60 + '\r')
+    sys.stdout.flush()
+    if info:
+        print(info)
+    sys.stdout.write(('[%-50s]%.2f%%' % (('>' * count), portion / total * 100))+'\r')
+    sys.stdout.flush()
+    if portion >= total:
+        sys.stdout.write('\n')
+        return True
+
 # 输出扫描结果
 def oneScan(url):
     #lock.acquire()
@@ -115,10 +129,17 @@ def oneScan(url):
 def main():
     url = mainPage()
     urls = urlSplit(url)
+    threads = []
+    executor = ThreadPoolExecutor(max_workers=thread)
+    portion = 0
+    total = len(urls)
+    print("\033[34m{} {} target {} {} threads\033[0m".format("[*]", str(total), "\n[*]", str(thread)))
     for info in executor.map(oneScan, urls):
-        if info:
-            print(info)
-    print("Down.")
+        portion += 1
+        sum = progress_bar(portion, total, info)
+        if sum:
+            break
+    print("Done.")
 
 
 if __name__ == "__main__":
